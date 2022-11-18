05760af13c260ad63cbf66c4cba89d8343561aca
Chiro Liang <Chiro2001@163.com>
2022-11-18 09:23:38 +0800

    todo: fix dlopen failed: unknown reloc type 16

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -16,52 +16,16 @@ typedef unsigned __int128 UINT128;
 
 #define METHOD_COUNT_MAX 4
 
-ofstream OutFile;
-
 // 将val截断, 使其宽度变成bits
 #define truncate(val, bits) ((val) & ((1 << (bits)) - 1))
 
-class PredictionResult {
-public:
-  UINT64 takenCorrect = 0;
-  UINT64 takenIncorrect = 0;
-  UINT64 notTakenCorrect = 0;
-  UINT64 notTakenIncorrect = 0;
-  BOOL used = false;
-  string name = "unknown";
-};
-
-static PredictionResult results[METHOD_COUNT_MAX] = {};
-
 // This knob sets the output file name
-KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "bp.txt", "specify the output file name");
+// KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "bp.txt", "specify the output file name");
+// KnobOutputFile.Value().c_str()
 
 // This function is called when the application exits
 VOID Fini(int, VOID *v) {
-  for (int i = 0; i < METHOD_COUNT_MAX; i++) {
-    if (results[i].used) {
-      double precision = 100 * double(results[i].takenCorrect + results[i].notTakenCorrect) /
-                         (results[i].takenCorrect + results[i].notTakenCorrect + results[i].takenIncorrect +
-                          results[i].notTakenIncorrect);
-
-      cout << "name: " << results[i].name << endl
-           << "takenCorrect: " << results[i].takenCorrect << endl
-           << "takenIncorrect: " << results[i].takenIncorrect << endl
-           << "notTakenCorrect: " << results[i].notTakenCorrect << endl
-           << "nnotTakenIncorrect: " << results[i].notTakenIncorrect << endl
-           << "Precision: " << precision << endl;
-
-      OutFile.setf(ios::showbase);
-      OutFile << "name: " << results[i].name << endl
-              << "takenCorrect: " << results[i].takenCorrect << endl
-              << "takenIncorrect: " << results[i].takenIncorrect << endl
-              << "notTakenCorrect: " << results[i].notTakenCorrect << endl
-              << "nnotTakenIncorrect: " << results[i].notTakenIncorrect << endl
-              << "Precision: " << precision << endl;
-    }
-  }
-
-  OutFile.close();
+  // rust_finish();
 }
 
 /* ===================================================================== */
@@ -86,7 +50,8 @@ int main(int argc, char *argv[]) {
   // Initialize pin
   if (PIN_Init(argc, argv)) return Usage();
 
-  OutFile.open(KnobOutputFile.Value().c_str());
+  // rust_start();
+  std::cout << "rusty_cxxbridge_integer = " << rusty_cxxbridge_integer() << std::endl;
 
   // Register Instruction to be called to instrument instructions
   INS_AddInstrumentFunction(Instruction, nullptr);
@@ -100,35 +65,9 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-/**
- * Predict for one method
- * @param method method index
- * @param pc PC now
- * @param direction before / after
- * @return 0: not take, other value: jump PC
- */
-UINT64 predictBranchMethod(int method, ADDRINT pc, BOOL direction) {
-  // TODO
-  return 0;
-}
-
 // This function is called every time a control-flow instruction is encountered
 void predictBranch(ADDRINT pc, BOOL direction) {
-  for (int method = 0; method < METHOD_COUNT_MAX; method++) {
-    UINT64 prediction = predictBranchMethod(method, pc, direction);
-    BOOL take = prediction != 0;
-    if (take) {
-      if (direction)
-        results[method].takenCorrect++;
-      else
-        results[method].takenIncorrect++;
-    } else {
-      if (direction)
-        results[method].notTakenIncorrect++;
-      else
-        results[method].notTakenCorrect++;
-    }
-  }
+  // predict_branch(pc, direction);
 }
 
 // Pin calls this function every time a new instruction is encountered
--- lab2/src/rusty_code/src/lib.rs
+++ lab2/src/rusty_code/src/lib.rs
@@ -1,9 +1,19 @@
+use lazy_static::lazy_static;
+use mut_static::MutStatic;
+
 #[cxx::bridge]
 mod ffi {
     extern "Rust" {
         fn rusty_cxxbridge_integer() -> i32;
+        // fn predict_branch(pc: u64, direction: bool);
+        // fn rust_start() -> i32;
+        // fn rust_finish() -> i32;
     }
 }
+
+pub fn rust_start() -> i32 { 0 }
+
+pub fn rust_finish() -> i32 { 0 }
 
 pub fn rusty_cxxbridge_integer() -> i32 {
     42
@@ -12,4 +22,46 @@ pub fn rusty_cxxbridge_integer() -> i32 {
 #[no_mangle]
 pub extern "C" fn rusty_extern_c_integer() -> i32 {
     322
+}
+
+
+#[derive(Debug, Default, Copy, Clone)]
+pub struct TestResult {
+    pub taken_correct: u64,
+    pub taken_incorrect: u64,
+    pub not_taken_correct: u64,
+    pub not_taken_incorrect: u64,
+    pub used: bool,
+    pub name: &'static str,
+}
+
+impl TestResult {
+    pub fn name_me(&mut self, name: &'static str) -> &mut Self {
+        self.name = name;
+        self
+    }
+}
+
+const METHOD_COUNT_MAX: usize = 4;
+lazy_static! {
+    pub static ref RESULTS: MutStatic<Vec<TestResult>> = MutStatic::from(vec![TestResult::default(); METHOD_COUNT_MAX]);
+}
+
+pub fn predict_branch(pc: u64, direction: bool) {}
+
+fn print_result() {
+    for result in RESULTS.read().unwrap().iter() {
+        println!("{:?}", result);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{print_result, RESULTS};
+
+    #[test]
+    fn simple_test() {
+        RESULTS.write().unwrap()[0].name_me("Hi");
+        print_result()
+    }
 }
