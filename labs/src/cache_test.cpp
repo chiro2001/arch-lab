3b792b3d93c709663273fe878d1b81441fae48b0
Chiro Liang <Chiro2001@163.com>
2022-12-17 21:32:47 +0800

    L1 & L2 size test

--- labs/src/cache_test.cpp
+++ labs/src/cache_test.cpp
@@ -160,29 +160,28 @@ void Test_Cache_Size() {
   printf("Cache Size Test\n");
 
   vector<pair<size_t, double>> time;
-  // for (auto i = 5; i <= 11; i++) {
-  // for (auto i = 1; i < 5; i++) {
-  //   size_t sz = KiB(1 << i);
-  //   time.emplace_back(sz, visit_array_in_size(sz, 0x1000 * 4));
-  //   display_pair_result(time.back());
-  //   sz = (size_t) ((double) (sz) * 1.5);
-  //   time.emplace_back(sz, visit_array_in_size(sz, 0x1000 * 4));
-  //   display_pair_result(time.back());
-  // }
-  // for (auto i = 5; i <= 7; i++) {
-  //   size_t sz = KiB(1 << i);
-  //   time.emplace_back(sz, visit_array_in_size(sz));
-  //   display_pair_result(time.back());
-  //   sz = (size_t) ((double) (sz) * 1.5);
-  //   time.emplace_back(sz, visit_array_in_size(sz));
-  //   display_pair_result(time.back());
-  // }
-  for (auto i = 8; i <= 11; i++) {
+  for (auto i = 1; i < 5; i++) {
     size_t sz = KiB(1 << i);
-    time.emplace_back(sz, visit_array_in_size(sz, 0x40));
+    time.emplace_back(sz, visit_array_in_size(sz, 0x1000 * 4));
     display_pair_result(time.back());
     sz = (size_t) ((double) (sz) * 1.5);
-    time.emplace_back(sz, visit_array_in_size(sz, 0x40));
+    time.emplace_back(sz, visit_array_in_size(sz, 0x1000 * 4));
+    display_pair_result(time.back());
+  }
+  for (auto i = 5; i <= 7; i++) {
+    size_t sz = KiB(1 << i);
+    time.emplace_back(sz, visit_array_in_size(sz));
+    display_pair_result(time.back());
+    sz = (size_t) ((double) (sz) * 1.5);
+    time.emplace_back(sz, visit_array_in_size(sz));
+    display_pair_result(time.back());
+  }
+  for (auto i = 8; i <= 11; i++) {
+    size_t sz = KiB(1 << i);
+    time.emplace_back(sz, visit_array_in_size(sz));
+    display_pair_result(time.back());
+    sz = (size_t) ((double) (sz) * 1.5);
+    time.emplace_back(sz, visit_array_in_size(sz));
     display_pair_result(time.back());
   }
 
