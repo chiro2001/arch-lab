88ca4f0f3193ece5dcb14571a2130a5a078dc82d
Chiro Liang <Chiro2001@163.com>
2023-01-05 23:34:50 +0800

    Test_L1C_Way_Count method 2

--- labs/src/cache_test.cpp
+++ labs/src/cache_test.cpp
@@ -197,11 +197,16 @@ double visit_array_way(size_t w) {
   struct timeval tp[2];
   Clear_L2_Cache();
   register uint32_t access = 0;
+  size_t size = KiB(48) << 1;
+  size_t blk = size >> w;
   gettimeofday(&tp[0], nullptr);
-  for (register int k = 0; k < loop_const / 128; k++) {
-    for (register uint32_t index = 0; index < (1 << w); index += 1) {
-      array[(index << 15) & (ARRAY_SIZE - 1)] = index + 1;
-      access++;
+  for (register int i = 1; i < loop_const / 8; i++) {
+    for (register int k = 1; k < (1 << w); k += 2) {
+      size_t s = k * blk;
+      for (register uint32_t index = s; index < s + blk; index += 1) {
+        array[index] = index + 1;
+        access++;
+      }
     }
   }
   gettimeofday(&tp[1], nullptr);
@@ -215,14 +220,14 @@ void Test_L1C_Way_Count() {
 
   vector<pair<size_t, double>> time;
   size_t level0 = 1;
-  size_t level1 = 9;
+  size_t level1 = 7;
   // warm up
   for (auto i = level0; i < level1; i++) {
     size_t m = 1 << i;
     visit_array_way(m);
   }
   for (auto i = level0; i < level1; i++) {
-    size_t m = 1 << i;
+    size_t m = 1 << (i - 1);
     time.emplace_back(m, visit_array_way(i));
     // display_block_result(time.back());
     // m = (size_t) ((double) (m) * 1.5);
@@ -297,14 +302,14 @@ void Test_TLB_Size() {
 FILE *log_fp = nullptr;
 
 int main() {
-  Test_Cache_Size();
-  Test_L1C_Block_Size();
+  // Test_Cache_Size();
+  // Test_L1C_Block_Size();
   // Test_L2C_Block_Size();
   Test_L1C_Way_Count();
   // Test_L2C_Way_Count();
   // Test_Cache_Write_Policy();
   // Test_Cache_Swap_Method();
-  Test_TLB_Size();
+  // Test_TLB_Size();
 
   return 0;
 }
